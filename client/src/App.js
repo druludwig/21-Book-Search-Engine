6d6f2e1d88303cf55fe301619a4267e3b3a48a9f
Dru Ludwig <druludwig@yahoo.com>
2021-09-09 16:38:41 -0700

    Apollo fix

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context'
-import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
   request: (operation) => {
